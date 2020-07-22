a9f4f4a22b93040ad4ca4f055dd85e1e66cb5786
angelaranda200 <angelaranda2011@hotmail.com>
2020-07-22 18:09:57 -0500

    practica de git

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div>
+    <table>
+      <tbody>
+         <tr v-for="todo in todos" :key="todo.id">
+          <td>{{todo.id}}</td>
+          <td>{{todo.name}}</td>
+          <td>{{todo.username}}</td>
+          <td>{{todo.email}}</td>
+          <td>{{todo.phone}}</td>
+         </tr>
+      </tbody>
+    </table>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios  from 'axios'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+ data() {
+   return {
+     todos:null
+   }
+ },
+ mounted() {
+   console.log('Hola mundo desde mounted')
+   this.getTodos()
+
+ },
+ methods: {
+   getTodos(){
+     console.log('aca va el codigo getTodos')
+     axios.get('https://jsonplaceholder.typicode.com/users')
+     .then(response => {
+            this.todos = response.data
+     }).catch(e => console.log(e))
+
+   }
+ },
 }
 </script>
 
